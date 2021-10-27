8769d19610bffcde0d0b2cd8b8431c08e1414ff9
mhmtnzly <mhmtnzly@gmail.com>
2021-10-27 22:05:43 +0200

    v.0.2

--- client.py
+++ client.py
@@ -6,22 +6,20 @@ class Client:
         self.name=name
         self.total_amount=total_amount
         self.account_number=random_list[Client.i]
-        self.account={'name':self.name,'total_amount':self.total_amount,"account_number":self.account_number}
         Client.i+=1
     def withdrawn(self,amount):
-        if self.account["total_amount"]>=amount:
-            self.account["total_amount"]-=amount
+        if self.total_amount>=amount:
+            self.total_amount-=amount
             print("The sum of {} has been withdrawn from your account balance.".format(amount))
-            print("Your current account balance is: {}".format(self.account["total_amount"]))
-            return self.account['total_amount']
+            self.current_balance()
+            return self.total_amount
         else:
             print("Your current account balance is not enough.")
-            return self.account['total_amount']
     def deposit(self,amount):
-        self.account["total_amount"]+=amount
+        self.total_amount+=amount
         print("The sum of {} has been added to your account balance.".format(amount))
-        print("Your current account balance is: {}".format(self.account['total_amount']))
-        return self.account['total_amount']
+        self.current_balance()
+        return self.total_amount
 
     def current_balance(self):
         print ("Your current account balance is: {}".format(self.total_amount))
--- main.py
+++ main.py
@@ -1,53 +1,57 @@
 from client import Client
 from bank import Bank
-class main(Bank):    
-    b1=Bank(input('Name of Bank: '))
-    import time
+import time
+
+b1=Bank(input('Name of Bank: '))
+
+available=True   
+while available:
     print("Welcome to {}".format(b1.name))
-    while True:
-        print("""Choose an option:
+    print("""Choose an option:
         1. Open new bank account
         2. Open existing bank account
         3. Exit
         """)
-        choice=input("")
-        if choice=='3':
-            break
-        
-        elif choice=='1':
-            name_=input("Name: ")
-            t_amount=int(input("Total amount: "))
-            client1=Client(name_, t_amount)
-            b1.add_clients(client1.account)
+    choice=int(input("1,2,3: "))
+    if choice==3:
+            available = False
+    elif choice==1:
+        print("To create an account, please fill in the information below.")
+        name_=input("Name: ")
+        t_amount=int(input("Total amount: "))
+        client1=Client(name_, t_amount)
+        b1.add_clients(client1)
+        time.sleep(1)
+        print("Account created successfully! Your account number is: {}".format(client1.account_number))
+    elif choice==2:
+        print("To access your account, please enter your credentials below.")            
+        account_number=int(input("Account number: "))
+        name=input("Name: ")
+        client_authentication=b1.authentication(name,account_number)
+        if client_authentication:
+            authorized = True
+            print("Bank account is opening...")
             time.sleep(1)
-            print("Account created successfully! Your account number is: {}".format(client1.account['account_number']))
-        elif choice=='2':
-            account_number=int(input("Account number: "))
-            name=input("Name: ")
-            client_authentication=b1.authentication(name,account_number)
-            if client_authentication:
-                print("Bank account is opening...")
-                time.sleep(1)
-                print('Welcome {}'.format(name))
-                for i in range(len(b1.getclients())):
-                    if b1.getclients()[i]['account_number']==account_number:
-                        break
-                while True:
-                    print("""Choose an option:
+            print('Welcome {}'.format(name))
+            while authorized:
+                print("""Choose an option:
             1. Withdraw
             2. Deposit
             3. Balance
             4. Exit""")
-                    choice1=input("Choice: ")
-                    if choice1=='1':
-                        b1.getclients()[i]['total_amount']=Client(b1.getclients()[i]['name'],b1.getclients()[i]['total_amount']).withdrawn(int(input("Withdrawn amount: ")))                    
-                    elif choice1=='2':
-                        b1.getclients()[i]['total_amount']=Client(b1.getclients()[i]['name'],b1.getclients()[i]['total_amount']).deposit(int(input("Deposit amount: ")))
-                    elif choice1=='3':
-                        Client(b1.getclients()[i]['name'],b1.getclients()[i]['total_amount']).current_balance()
-                    elif choice1=="4":
-                        break
-            else:
-                time.sleep(1)
-                print('Authentication failed! \nReason: account is not found')
-m=main()
+                choice1=int(input("Choice: "))
+                if choice1==1:
+                    w_amount = int(input("Withdraw amount:"))
+                    client_authentication.withdrawn(w_amount)
+                elif choice1==2:
+                    d_amount = int(input("Deposit amount:"))
+                    client_authentication.deposit(d_amount)
+                elif choice1==3:
+                    client_authentication.current_balance()
+                elif choice1==4:
+                    authorized=False
+            
+        else:
+            time.sleep(1)
+            print('Authentication failed! \nReason: account is not found')
+        
